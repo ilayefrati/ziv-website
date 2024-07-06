bd33384a278a9859dddebea8a0d394b79967aaa9
Ilay Efrati <ilay2092003@gmail.com>
2024-07-07 00:01:06 +0300

    finished the first page in the main section (not including the navbar)

---
+++ src/MainFirstPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './MainFirstPage.css';
+
+function MainFirstPage(){
+    return(
+        <div className="main-first-page">
+            <div className="main-first-page-text">
+                <h1>זיו מערכות תוכנה</h1>
+                <h3>מערכת זיו מספקת פתרון מקיף לניהול צי הרכב ומאפשרת לחתוך בעלויות תפעול ולחסוך בהוצאות מיותרות. 
+                    כמו כן מבצעת בקרה ומעקב אחר דוחות, טיפולים לרכב, רישיונות, בדיקות תקופתיות ועוד 
+                </h3>
+            </div>
+        </div>
+    )
+}
+
+export default MainFirstPage;
