9a1eaee577aa9fc96b30f6da3a4b89544991a451
ilay efrati <ilay2092003@gmail.com>
2024-07-16 20:33:21 +0300

    added basic routing using react router, now i need to add the Navbar itself

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 import MainFirstPage from "./MainFirstPage";
 
 function App() {
   return (
-    <div className="App">
-      <MainFirstPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainFirstPage/>}></Route>
+        <Route path="/about" element={<MainFirstPage/>}></Route>
+        <Route path="/contact" element={<MainFirstPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
