0f446ce784558184a491e41e111bb19d8f0ad940
ilay efrati <ilay2092003@gmail.com>
2024-07-22 22:56:19 +0300

    added the TitleAndLines component

---
+++ src/TitleAndLines.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TitleAndLines.css";
+
+function TitleAndLines() {
+  return (
+    <div className="title-and-lines">
+      <hr className="left-line" />
+      <h2 className="title">יתרונות התוכנה</h2>
+      <hr className="right-line" />
+    </div>
+  );
+}
+
+export default TitleAndLines;
