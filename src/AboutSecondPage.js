65f6e50441d74670808f528a1c7deb1317a8e194
Ilay Efrati <ilay2092003@gmail.com>
2024-07-27 22:01:18 +0300

    created the AdvantageContainer component and the AboutSecondPage component. changed some small things in index.css

---
+++ src/AboutSecondPage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./AboutSecondPage.css";
+import TitleAndLines from "./TitleAndLines";
+import AdvantageContainer from "./AdvantageContainer";
+
+function AboutSecondPage() {
+  return (
+    <div className="about-second-page">
+      <TitleAndLines />
+      <div className="advantages">
+        <AdvantageContainer
+          link="/"
+          icon="fa-solid fa-briefcase"
+          title="ניהול תיקי רכב"
+          text="מערכת זיו מאפשרת לנהל תיק רכב מפורט לכל הרכבים בצי"
+        />
+        <AdvantageContainer
+          link="/"
+          icon="fa-solid fa-briefcase"
+          title="ניהול תיקי רכב"
+          text="מערכת זיו מאפשרת לנהל תיק רכב מפורט לכל הרכבים בצי"
+        />
+        <AdvantageContainer
+          link="/"
+          icon="fa-solid fa-briefcase"
+          title="ניהול תיקי רכב"
+          text="מערכת זיו מאפשרת לנהל תיק רכב מפורט לכל הרכבים בצי"
+        />
+        <AdvantageContainer
+          link="/"
+          icon="fa-solid fa-briefcase"
+          title="ניהול תיקי רכב"
+          text="מערכת זיו מאפשרת לנהל תיק רכב מפורט לכל הרכבים בצי"
+        />
+        <AdvantageContainer
+          link="/"
+          icon="fa-solid fa-briefcase"
+          title="ניהול תיקי רכב"
+          text="מערכת זיו מאפשרת לנהל תיק רכב מפורט לכל הרכבים בצי"
+        />
+        <AdvantageContainer
+          link="/"
+          icon="fa-solid fa-briefcase"
+          title="ניהול תיקי רכב"
+          text="מערכת זיו מאפשרת לנהל תיק רכב מפורט לכל הרכבים בצי"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default AboutSecondPage;
--- src/AdvantageContainer.js
+++ src/AdvantageContainer.js
@@ -1,6 +1,18 @@
 import React from "react";
-import './AdvantageContainer.css';
+import "./AdvantageContainer.css";
+import { Link } from "react-router-dom";
 
-function AdvantageContainer(){
-    
+function AdvantageContainer(props) {
+  return (
+    <Link to={props.link}>
+      <div className="advantage-container">
+        <i className={props.icon}></i>
+        <h3>{props.title}</h3>
+        <p>{props.text}</p>
+        <h4>למידע נוסף &gt;</h4>
+        </div>
+    </Link>
+  );
 }
+
+export default AdvantageContainer;
