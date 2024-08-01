4ba4ff900444d4d3daab1d84cfaa0b909d7bd66c
Ilay Efrati <ilay2092003@gmail.com>
2024-08-01 23:54:50 +0300

    fixed additional info in AdvantageContainer to be always on the buttom. cut the ziv logo image to make an equal space in the Navbar. expended the AboutFirst page image vertically so it wouldnt touch the Navbar. fixed the problem where the Link didnt take me to the top of the page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./Main";
 import About from "./About";
-import AdvantagesPage from './AdvantagesPage';
+import AdvantagesPage from "./AdvantagesPage";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+      {/* the ScrollToTop component renders initially and gets the path from the getlocation hook.
+       afterwards, it initializes the window position to 0,0 at the start and every time the route changes - using the useEffect hook. */}
       <Routes>
-        <Route path="/" element={<Main/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/about" element={<About />}></Route>
         <Route path="/about/advantages/:id" element={<AdvantagesPage />} />
-        <Route path="/contact" element={<Main/>}></Route>
+        <Route path="/contact" element={<Main />}></Route>
       </Routes>
     </BrowserRouter>
   );
